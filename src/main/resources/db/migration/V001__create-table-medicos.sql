18c337ffb51be570ca99fbdaf5eeb906a5d31aa3
Manoel Lima <manoel.lima@manaus.am.gov.br>
2025-12-01 11:09:13 -0400

    feat: enhance Medico registration with validation and add telefone field

---
+++ src/main/resources/db/migration/V001__create-table-medicos.sql
@@ -0,0 +1,15 @@
+create table medicos
+(
+    id            serial primary key,
+    nome          varchar(100) not null,
+    email         varchar(100) not null,
+    crm           varchar(20)  not null,
+    especialidade varchar(50)  not null,
+    logradouro    varchar(100) not null,
+    bairro        varchar(100) not null,
+    cep           varchar(9)   not null,
+    complemento   varchar(100),
+    numero        varchar(20),
+    uf            char(2)      not null,
+    cidade        varchar(100) not null
+);
